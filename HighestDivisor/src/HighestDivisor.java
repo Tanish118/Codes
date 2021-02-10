96d9108c2266f5d9e9c643d720e642ba54a6d30f
Tanish Sharma <Tanish118@users.noreply.github.com>
2021-02-10 22:48:32 +0530

    highest divisor

--- HighestDivisor/src/HighestDivisor.java
+++ HighestDivisor/src/HighestDivisor.java
@@ -2,10 +2,18 @@ import java.util.*;
 public class HighestDivisor {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int test=sc.nextInt();
-        while(test!=0){
-
-            test--;
+        int num=sc.nextInt();
+        if(num<=10)
+            System.out.println(num);
+        else{
+            int ans=0;
+            for(int i=10;i>0;i--){
+                if(num%i==0){
+                    ans=i;
+                    break;
+                }
+            }
+            System.out.print(ans);
         }
     }
 }
